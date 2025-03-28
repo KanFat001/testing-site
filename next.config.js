971213d003bce120a1da127e3fa352e8cfc0f4dc
FarhanAli04 <farhan.xwave@gmail.com>
2025-03-28 10:24:33 +0500

    no

--- next.config.js
+++ next.config.js
@@ -1,22 +1,51 @@
 /** @type {import('next').NextConfig} */
 const runtimeCaching = require("next-pwa/cache");
 const withPWA = require('next-pwa');
-const { hostname } = new URL(`${process.env.NEXT_PUBLIC_BASE_URL}`);
-const nextConfig={
+
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+const { hostname } = new URL(baseUrl);
+
+const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
     domains: [`${hostname}`]
   },
+  webpack: (config, { isServer }) => {
+    config.optimization = {
+      ...config.optimization,
+      minimize: !isServer,
+      splitChunks: {
+        chunks: 'all',
+        cacheGroups: {
+          default: false,
+          vendors: false,
+          commons: {
+            name: 'commons',
+            chunks: 'all',
+            minChunks: 2,
+            reuseExistingChunk: true
+          }
+        }
+      }
+    };
+    return config;
+  },
+  experimental: {
+    esmExternals: 'loose',
+    optimizeCss: true
+  }
 }
-const pwa = process.env.NEXT_PWA_STATUS;
-const nextConfigWithPwa = withPWA({
+
+const pwaConfig = {
   dest: "public",
   register: true,
   skipWaiting: true,
   runtimeCaching,
   disable: process.env.NODE_ENV === "development",
-}) (
-    nextConfig
-);
-module.exports = pwa==='1'?nextConfigWithPwa:nextConfig ;
+  buildExcludes: [/middleware-manifest\.json$/],
+  maximumFileSizeToCacheInBytes: 3000000
+};
+
+const nextConfigWithPwa = withPWA(pwaConfig)(nextConfig);
+module.exports = process.env.NEXT_PWA_STATUS === '1' ? nextConfigWithPwa : nextConfig;
